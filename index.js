a60719e16d21378957f1e23bb4ab12e2196938b7
Nicole Bradford <nbradf2@gmail.com>
2018-08-02 19:32:19 -0500

    add onClick function to menu bars to show drop-down nav

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+	$('.fa-bars').click(function(e) {
+		e.preventDefault();
+		// console.log('you clicked nav bars');
+		$('.drop-nav').html(
+			'<div class="drop-links">' +
+				'<li><a href="#home" class="links link-home">Home</a></li>' +
+	         	'<li><a href="#recent-work" class="links link-recent-work">Recent Work</a></li>' + 
+	          	'<li><a href="#get-in-touch" class="links link-contact">Get in Touch</a></li>' +
+          	'</div>'
+		)
+	})
+
+
 	let d = new Date;
 	let year = d.getFullYear();
 	$("#footer").append('<p>&copy; 2018 Nicole Bradford</p>')
