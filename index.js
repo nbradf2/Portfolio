1fe9d12bcf811c3dda36dc2ed006157801c8847a
Nicole Bradford <nbradf2@gmail.com>
2018-08-04 14:09:48 -0500

    add toggle function to responsive menu

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
 
-	$('.fa-bars').click(function(e) {
+	$('.fas').click(function(e) {
 		e.preventDefault();
-		// console.log('you clicked nav bars');
-		$('.drop-nav').html(
-			'<div class="drop-links">' +
-				'<li><a href="#home" class="links link-home">Home</a></li>' +
-	         	'<li><a href="#recent-work" class="links link-recent-work">Recent Work</a></li>' + 
-	          	'<li><a href="#get-in-touch" class="links link-contact">Get in Touch</a></li>' +
-          	'</div>'
-		)
+		$('.drop-nav').toggle();
 	})
 
 
